1b95180809e079c09b054d21f372b6b23d898c37
Douglas Daniel <douglashdaniel@gmail.com>
2022-07-13 11:09:15 -0600

    Initialized App

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import BraveLogo from './assets/brave.svg'
+
+function App () {
+  return (
+    <div className="App">
+      <div>
+        <img src={BraveLogo} className="logo react" alt="Brave logo" />
+      </div>
+    </div>
+  )
+}
+
+export default App
